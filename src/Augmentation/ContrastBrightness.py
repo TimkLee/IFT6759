8c8968abe449f868e7fd56d794de97556237e428
TimKLee <timothykolee@gmail.com>
2022-03-25 14:12:05 -0400

    Implemented the simple augmentation methods using PyTorch built-in functions and methods.

--- src/Augmentation/ContrastBrightness.py
+++ src/Augmentation/ContrastBrightness.py
@@ -3,15 +3,35 @@ Apply contrast adjustment, brightness adjustment, or both at the same time with 
 Contrast adjustment is performed using the `adjust_contrast` function in PyTorch, the `contrast_factor` is selected to be in the range of [0.5, 1.5].
 Brightness adjustment is performed using the `adjust_brightness` function in PyTorch, the `brightness_factor` is selected to be in the range of [0.5, 1.5].
 
-
     Args:
-
+        data [Tensor]: input images with a size of [Batch size, Channel, Height, Width]
+        seed [int]: seed for randomization; default: "6759" 
 
     Returns:
+        aug_data [Tensor]: output images with a size of [4*Batch size, Channel, Height, Width], where the first [Batch size, Channel, Height, Width] is the original data
 
 """
 
+import torch
+import torchvision
+import torchvision.transforms.functional as F
 
-def Aug():
+def Aug(data,seed = 6759):
     
-    return print("Augmented")
+    torch.manual_seed(seed)
+    prob = torch.rand(3)
+    aug_data = data
+    
+    for i in range(len(prob)):
+        
+        if prob[i]<0.3333: 
+            aug_data = torch.cat((aug_data,F.adjust_brightness(data, brightness_factor=torch.FloatTensor(1,).uniform_(0.5, 1.5))))
+            
+        elif prob[i]>0.6666:
+            aug_data = torch.cat((aug_data,F.adjust_contrast(data, contrast_factor=torch.FloatTensor(1,).uniform_(0.5, 1.5))))
+        
+        else:
+            temp = F.adjust_brightness(data, brightness_factor=torch.FloatTensor(1,).uniform_(0.5, 1.5))
+            aug_data = torch.cat((aug_data,F.adjust_contrast(temp, contrast_factor=torch.FloatTensor(1,).uniform_(0.5, 1.5))))    
+    
+    return aug_data
--- src/Augmentation/Translation.py
+++ src/Augmentation/Translation.py
@@ -2,14 +2,35 @@
 Apply horizontal translation, a vertical translation, or both at the same time with equal probability. 
 The image is shifted by a pixel value between [-16, 16]. 
 
-
     Args:
-
+        data [Tensor]: input images with a size of [Batch size, Channel, Height, Width]
+        seed [int]: seed for randomization; default: "6759" 
 
     Returns:
+        aug_data [Tensor]: output images with a size of [4*Batch size, Channel, Height, Width], where the first [Batch size, Channel, Height, Width] is the original data
 
 """
 
-def Aug():
+import torch
+import torchvision
+import torchvision.transforms.functional as F
+
+def Aug(data,seed = 6759):
     
-    return print("Augmented")
+    torch.manual_seed(seed)
+    prob = torch.rand(3)
+    aug_data = data
+    
+    for i in range(len(prob)):
+        
+        if prob[i]<0.3333: 
+            aug_data = torch.cat((aug_data,F.affine(data,translate=[torch.randint(-16, 16,(1,)),0],angle=0,scale=1,shear=0)))
+            
+        elif prob[i]>0.6666:
+            aug_data = torch.cat((aug_data,F.affine(data,translate=[0,torch.randint(-16, 16,(1,))],angle=0,scale=1,shear=0)))
+        
+        else:
+            temp = F.vflip(data)
+            aug_data = torch.cat((aug_data,F.affine(data,translate=[torch.randint(-16, 16,(1,)),torch.randint(-16, 16,(1,))],angle=0,scale=1,shear=0)))
+    
+    return aug_data
