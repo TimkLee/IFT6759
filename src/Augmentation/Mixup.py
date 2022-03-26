9af300245018a7172b2c84d7800486f2159725a2
Krishna Maneesha Dendukuri <krishnamaneesha_ph@srmuniv.edu.in>
2022-03-26 19:35:07 -0400

    added mixup - inputs labels too-change required

--- src/Augmentation/Mixup.py
+++ src/Augmentation/Mixup.py
@@ -9,6 +9,20 @@ The strength parameter is selected in the range of [0, 1].
 
 """
 
-def Aug():
+def Aug(data,labels, seed = 6759):
+    torch.manual_seed(seed)
+    alpha = 0.4
+
+    #sample alpha from beta distribution
+    get_alpha = np.random.beta(alpha, alpha, labels.size(0))
+    get_alpha = np.concatenate([get_alpha[:,None], 1-get_alpha[:,None]], 1).max(1)
+    get_alpha = data.new(get_alpha)
+
+    #shuffle batch to get the second image to augment
+    shuffle = torch.randperm(data.size(0)).to(data.device)
+    x1, y1 = data[shuffle], labels[shuffle]
+
+    aug_data = (last_input * get_alpha.view(get_alpha.size(0),1,1,1) + x1 * (1-get_alpha).view(get_alpha.size(0),1,1,1))
+    aug_labels = labels * get_alpha + y1 * (1-get_alpha)
     
-    return print("Augmented Mixup")
+    return (aug_data , aug_labels)
