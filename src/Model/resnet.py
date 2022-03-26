6d3d72b54945ab645f45f7862029ed626806f98d
Yassir <yassmams16@gmail.com>
2022-03-26 16:16:55 -0400

    Resnet class implemented by reusing torchvision module

--- src/Model/resnet.py
+++ src/Model/resnet.py
@@ -1,55 +1,50 @@
-import os
-import random
-
+from torchvision.models import resnet18
 import numpy as np
-import torch
-import h5py
-from torch.utils.data import Dataset, DataLoader
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
+
+class ModelClass(nn.Module):
+
+    def __init__(self,num_classes=10):
+      """
+      reuse of torchvision module : https://pytorch.org/vision/stable/generated/torchvision.models.resnet18.html
+      """
+      super(ModelClass,self).__init__()
+      self.resnet18 = resnet18(progress=False,num_classes=num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+      """
+      input : x of shape (batch size, channel , height, width)
+      output : y_pred, output after forward pass into resnet18 block. With shape 
+      """
+      y_pred = self.resnet18(x)
+      return y_pred
+
+    def train_sup_up(self, epoch, dataloader, optimizer, criterion):
+        """
+        TRAIN LOOP FOR SUPERVISED/UNSUPERVISED LEARNING
+        Train the model with the given criterion, optimizer, dataloader for given epochs. Results propogated to WANDB for visualization
+        """
+        #TODO
+        pass
+
+    def train_shot(self, epoch, dataloader, optimizer, criterion):
+        """
+        TRAIN LOOP FOR FEWSHOT/ZEROSHOT LEARNING
+        Train the model with the given criterion, optimizer, dataloader for given epochs. Results propogated to WANDB for visualization
+        """
+        #TODO
+        pass
 
 
-import torch.backends.cudnn as cudnn
-import torch.nn as nn
-import torch.nn.functional as F
-
-class ResNet(nn.Module):
-  def __init__(self, block, num_blocks, num_classes=10):
-    super(ResNet, self).__init__()
-    self.in_planes = 64
-
-    self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-    self.bn1 = nn.BatchNorm2d(64)
-    self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-    self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-    self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-    self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-    self.linear = nn.Linear(512*block.expansion, num_classes)
-
-  def _make_layer(self, block, planes, num_blocks, stride):
-    strides = [stride] + [1]*(num_blocks-1)
-    layers = []
-    for stride in strides:
-      layers.append(block(self.in_planes, planes, stride))
-      self.in_planes = planes * block.expansion      
-    return nn.Sequential(*layers)
-
-  def forward(self, x):
-    out = F.relu(self.bn1(self.conv1(x)))
-    out = self.layer1(out)
-    out = self.layer2(out)
-    out = self.layer3(out)
-    out = self.layer4(out)
-    out = F.avg_pool2d(out, 4)
-    out = out.view(out.size(0), -1)
-    out = self.linear(out)
-    return out
-
-def get_criterion(self):
-    return nn.CrossEntropyLoss()
-
-
+    @torch.no_grad()
+    def evaluation():
+        """
+        Evaluate the model for various evaluation metrics. Results propogated to WANDB for visualization 
+        """
+        #TODO
+        pass
 
 
