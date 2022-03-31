7f9ac0d160f69c38bc4851b1784139dff281207f
Krishna Maneesha Dendukuri <krishnamaneesha_ph@srmuniv.edu.in>
2022-03-31 13:39:09 -0400

    implement cutout v1 -to do:optimize loop& resolve seed

--- src/Augmentation/Cutout.py
+++ src/Augmentation/Cutout.py
@@ -13,6 +13,36 @@ Select a value in the range of [1, 8], this value times 2 represents the height 
 """
 
 
-def Aug():
+def Aug(data, labels, seed = 6759):
+    # torch.manual_seed(seed)
+
+    # setting seed here would cutout the same portion of the image everytime. which is not the intended behaviour
+    # TODO: reproducibility vs algo tradeoff
+    np.random.seed(seed)
+
+    h = data.shape[2]
+    w = data.shape[3]
     
-    return print("Augmented Cutout")
+    # Using default settings in official implementation: 
+    n_holes = 1
+    length = 16
+
+    aug_data = data
+    mask = np.ones((h, w), np.float32)
+
+    for i, img in enumerate(data):
+        y = np.random.randint(h)
+        x = np.random.randint(w)
+
+        y1 = np.clip(y - length // 2, 0, h)
+        y2 = np.clip(y + length // 2, 0, h)
+        x1 = np.clip(x - length // 2, 0, w)
+        x2 = np.clip(x + length // 2, 0, w)
+
+        mask[y1: y2, x1: x2] = 0.
+
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        aug_data[i] = img * mask
+    
+    return (aug_data, labels)
