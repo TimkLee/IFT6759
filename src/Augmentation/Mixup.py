666a25d1a9e8bfa4dbc23c31dd81f6ca948d352a
Krishna Maneesha Dendukuri <krishnamaneesha_ph@srmuniv.edu.in>
2022-04-08 17:19:05 -0400

    updated mixup

--- src/Augmentation/Mixup.py
+++ src/Augmentation/Mixup.py
@@ -3,11 +3,17 @@ Select a random image and overlay the two images. A strength parameter is used t
 The strength parameter is selected in the range of [0, 1].
 
     Args:
-
+    data [Tensor]: input images with a size of [Batch size, Channel, Height, Width]
+    labels [Tensor]: input labels with a size of [Batch size, ]
+    seed [int]: seed for randomization; default: "6759"
 
     Returns:
+    aug_data [Tensor]: output images with a size of [2*Batch size, Channel, Height, Width]
+    aug_labels [Tensor]: onehot encoded output labels with a size of [2*Batch size,]
 
 """
+import torch
+import numpy as np
 
 def Aug(data,labels, seed = 6759):
     torch.manual_seed(seed)
@@ -24,5 +30,8 @@ def Aug(data,labels, seed = 6759):
 
     aug_data = (last_input * get_alpha.view(get_alpha.size(0),1,1,1) + x1 * (1-get_alpha).view(get_alpha.size(0),1,1,1))
     aug_labels = labels * get_alpha + y1 * (1-get_alpha)
+
+    aug_data = torch.cat((data, aug_data))
+    aug_labels = torch.cat((labels, aug_labels))
     
     return aug_data , aug_labels
