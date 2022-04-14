a1c76f465e9df59ee1cdc720a025e231c9cccaf9
Krishna Maneesha Dendukuri <krishnamaneesha_ph@srmuniv.edu.in>
2022-04-14 19:01:16 -0400

    bug fixes

--- src/Augmentation/Mixup.py
+++ src/Augmentation/Mixup.py
@@ -23,12 +23,13 @@ def Aug(data,labels):
     get_alpha = np.random.beta(alpha, alpha, labels.size(0))
     get_alpha = np.concatenate([get_alpha[:,None], 1-get_alpha[:,None]], 1).max(1)
     get_alpha = data.new(get_alpha)
+    get_alpha = get_alpha.unsqueeze(1)
 
     #shuffle batch to get the second image to augment
     shuffle = torch.randperm(data.size(0)).to(data.device)
     x1, y1 = data[shuffle], labels[shuffle]
-
-    aug_data = (last_input * get_alpha.view(get_alpha.size(0),1,1,1) + x1 * (1-get_alpha).view(get_alpha.size(0),1,1,1))
+    
+    aug_data = (data * get_alpha.view(get_alpha.size(0),1,1,1) + x1 * (1-get_alpha).view(get_alpha.size(0),1,1,1))
     aug_labels = labels * get_alpha + y1 * (1-get_alpha)
 
     aug_data = torch.cat((data, aug_data))
